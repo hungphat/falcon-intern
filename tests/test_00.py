122f6eb777c7f058d39795c20dfc9d4d57d0b1f1
Trang <trangtruong16101993@gmail.com>
2019-10-22 15:15:31 +0700

    trang aQA tc00

---
+++ tests/test_00.py
@@ -0,0 +1,18 @@
+from API_code.app import api
+from falcon import testing
+
+
+def setUpModule():    pass # nothing here for now
+def tearDownModule(): pass # nothing here for now
+
+
+class Test(testing.TestCase):
+
+    def setUp(self):    pass  # nothing here for now
+    def tearDown(self): pass  # nothing here for now
+
+    app = api
+
+    def test_tc00(self):
+        r = self.simulate_get('/health')
+        assert r.status_code == 200
