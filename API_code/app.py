7201c7ae46d0e48e649010a69890ea3ee662c2d9
Phat Future <hungphat0511@gmail.com>
2019-10-22 14:33:27 +0700

    TODO fixed

--- API_code/app.py
+++ API_code/app.py
@@ -1,10 +1,8 @@
 import falcon
 
-class TestResource:  #TODO dat ten tuong` minh thay vi Test
+class FalconLearn:
     def on_get(self, req, res):
         res.status = falcon.HTTP_200
-        res.body = ('200')  #TODO ko can
 
 api = falcon.API()
-test_resource = TestResource()  #TODO ko can bien test_resource, truyen tr.tiep TestResource() cho add_route()
-api.add_route('/health',test_resource)  #TODO sau phay fai co 1 kh.trang
+api.add_route('/health', FalconLearn())
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # falcon-intern
 
-#TODO lam sao de chay api nay?
+Trong terminal activate moi truong len:
+- source .venv/bin/activate
+Roi vao thu muc API_code:
+- cd API_code
+Dung gunicorn chay app.py (api) tren local host:
+- gunicorn app:api
+Dung Browser vao localhost:
+- 127.0.0.1/health
