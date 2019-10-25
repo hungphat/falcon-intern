e295452440158b6d04e487a3ca37f564038bdc67
Phat Future <hungphat0511@gmail.com>
2019-10-25 10:23:36 +0700

    pass testcase

--- API_code/app.py
+++ API_code/app.py
@@ -1,14 +1,26 @@
 import falcon
 import datetime
-from falcon import HTTPError
+from string import punctuation
 
 class HealthResource:
     def on_get(self, req, res):
         res.status = falcon.HTTP_200
 
 class HelloResource:
-    def on_get(self, req, resp, name):
-        resp.body   = (f'Hello {name}')
+    def on_get(self, req, resp, name=None):
+        try:
+            int(name)
+            resp.status = falcon.HTTP_404
+            resp.body = ('Interger not accept')
+        except ValueError:
+            count =0
+            for i in name:
+                count += punctuation.count(i)
+            if count >0:
+                resp.status = falcon.HTTP_404
+                resp.body = ('Interger not accept')
+            else:
+                resp.body = (f'Hello {name}')
 
 class HiResource:
     def on_get(self, req, resp):
@@ -21,23 +33,17 @@ class HiResource:
             resp.body = ('Good evening')
 
 class HolaResource:
-    def on_get(self, req, resp, name):
-        if name == None:            #TODO: làm sao để em lấy đuoc value name từ URL để em so sánh
+    def on_get(self, req, resp, name=None):
+        if name is None:
             resp.body = ('Must have name')
         else:
             resp.body = (f'Hello {name}')
-
-class HolaResourceException(HolaResource):
-    def handle(ex, req, resp, params):
-        raise falcon.HTTPError(falcon.HTTP_404)
-
-def error_handler(ex, req, resp, params):
-    resp.body = ('Http Error')
 
 
 api = falcon.API()
 api.add_route('/health', HealthResource())
 api.add_route('/hi', HiResource())
 api.add_route('/hello/{name}', HelloResource())
+api.add_route('/hello/', HelloResource())
 api.add_route('/hola/{name}', HolaResource())
-api.add_error_handler(Exception, error_handler)
+api.add_route('/hola/', HolaResource())
